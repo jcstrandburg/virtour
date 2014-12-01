1a8347f92d87009cb3e7c7f00b6edfd2f1ff43a7
Dylan Huizenga <dylzenga@gmail.com>
2014-12-01 08:42:20 -0800

    points on map now make sense

--- Virtour-Android/src/com/cs/wwu/csvirtualtour/MapTouchImageView.java
+++ Virtour-Android/src/com/cs/wwu/csvirtualtour/MapTouchImageView.java
@@ -5,16 +5,21 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 
 public class MapTouchImageView extends TouchImageView {
 
 	Paint p = new Paint();
 	float[] cords;
+	float borderx;
+	float bordery;
 	public MapTouchImageView(Context context, float[] coordinates) {
 		super(context);
 		cords = coordinates;
+		this.setZoom(1);
 	}
 	
 	private Boolean isInView(float xratio, float yratio)
@@ -52,8 +57,9 @@ public class MapTouchImageView extends TouchImageView {
 		return false;
 	}
 	
-	private float getZoomedPosition(float xratio, float yratio)
+	private PointF getZoomedPosition(float xratio, float yratio)
 	{
+		PointF returned = new PointF(0,0);
 		float xpos, ypos;
 		float imageWidth, imageHeight;
 		float zoomedtop, zoomedleft, zoomedbottom, zoomedright;
@@ -66,40 +72,39 @@ public class MapTouchImageView extends TouchImageView {
 		float yOffset = values[5];
 		
 		//get image width and height
-		imageWidth = (this.getWidth() - (2 * xOffset));
-		imageHeight = (this.getHeight() - (2 *yOffset));
+		imageWidth = (this.getMeasuredWidth());
+		imageHeight = (this.getMeasuredHeight());
+		
+		if (this.getCurrentZoom() == 1)
+		{
+			borderx = xOffset * 2;
+			bordery = yOffset * 2;
+		}
 		
 		//Get the unzoomed dot position
-		xpos = (imageWidth * xratio);
-		ypos = (imageHeight * yratio);
+		xpos = ((imageWidth - borderx) * xratio);
+		ypos = ((imageHeight - bordery) * yratio);
 		
-		//cacluate current image bounds
-		RectF bounds = this.getZoomedRect();
-		return 0.0f;
+
+		returned.x = (xpos) * this.getCurrentZoom() + xOffset;
+		returned.y = (ypos) * this.getCurrentZoom() + yOffset;
 		
+		return returned;
 	}
 	
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		p.setColor(Color.RED);
 		p.setStrokeWidth(2);
-		Rect bounds = this.getDrawable().getBounds();
-		Matrix matrix = this.getImageMatrix();
-		float[] values = new float[9];
-		matrix.getValues(values);
-		float x = values[2];
-		float y = values[5];
 		for (int i = 0; i < cords.length; i = i + 2)
 		{
 			if (isInView(cords[i], cords[i+1]))
 			{
-				canvas.drawCircle(x + ((this.getWidth() - (2 * x)) * cords[i]),y + (this.getHeight() - (2 * y))* cords[i+1],10 * this.getCurrentZoom() , p);
+				PointF zoomedLocation = getZoomedPosition(cords[i],cords[i+1]);
+				
+				canvas.drawCircle(zoomedLocation.x, zoomedLocation.y, 10 * this.getCurrentZoom(), p);
 			}
-		}
-		
-		isInView(cords[0],cords[1]);
-		
-		
+		}		
 	}
 
 
